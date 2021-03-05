6d50ba38e3b2436851af8859e0d65436eda2ff40
vrudas <vrudas4work@gmail.com>
2021-03-05 12:41:39 +0200

    #1 Removed manual src module declaration

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='djmag_top100_scraper',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    packages=find_packages(),
     install_requires=[
         'requests',
         'bs4',
