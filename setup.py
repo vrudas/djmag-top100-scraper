0ae9e3d3a650d7682cd763d7465716d84e862090
vrudas <vrudas4work@gmail.com>
2021-03-07 00:19:54 +0200

    #1 Restructure project according to https://docs.pytest.org/en/latest/goodpractices.html

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@
 from setuptools import find_packages, setup
 
 setup(
+    name='djmag_top100_scraper',
     packages=find_packages(),
-    package_dir={"": "src"},
+    package_dir={'': 'src'},
     install_requires=[
         'requests',
         'bs4',
