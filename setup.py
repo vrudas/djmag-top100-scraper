8b6267a2c7cdf9f7deaae35b3249b3034c2bfe68
vrudas <vrudas4work@gmail.com>
2021-03-06 15:26:05 +0200

    # Fixed execution of tests

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import find_packages, setup
 
 setup(
     name='djmag_top100_scraper',
-    packages=find_packages(),
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     install_requires=[
         'requests',
         'bs4',
