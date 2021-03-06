3bc6d1ae24ee8b517245640b3cd5578fd9740783
vrudas <vrudas4work@gmail.com>
2021-03-06 23:54:10 +0200

    #1 Fixed CI

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    packages=['src', 'tests'],
+    packages=find_packages(),
+    package_dir={"": "src"},
     install_requires=[
         'requests',
         'bs4',
